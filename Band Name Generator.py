622dccfef9f79bf41883b096e693c50dc511f5af
= <imachat001@gmail.com>
2022-05-17 22:40:56 +0100

    added files

---
+++ Band Name Generator.py
@@ -0,0 +1,7 @@
+print("WELCOME TO BAND NAME GENERATOR ")
+
+city = input("What is the name of your city?\n")
+
+pet = input("What is the name of a pet?\n")
+
+print("your band name could be " + city + " " + pet)
---
+++ main.py
@@ -0,0 +1,13 @@
+# testing the print function #
+
+print("Hello world!")
+
+print("what to print")
+
+# testing concatenation #
+
+print("roland" + " obiora")
+
+# input function #
+
+print(" hello " + input("what is your name?"))
